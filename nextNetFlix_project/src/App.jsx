5f775ae8b20c2cb842946a5f88419bb97573199c
Soli12 <kehaliabi@gmail.com>
2025-05-15 00:24:35 -0400

    header and footer

--- nextNetFlix_project/src/App.jsx
+++ nextNetFlix_project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './pages/Home/Home'
 import './App.css'
 
 function App() {
@@ -8,8 +7,8 @@ function App() {
 
   return (
    
-      <div>
-       <h1>hello</h1>
+      <div className='App'>
+       <Home/>
       </div>
       
   
